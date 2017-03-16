f17470a2f5ec436ec14617d1c9874829f09bbcaf
v-lai <lai.vivian@gmail.com>
2017-03-16 11:09:37 -0700

    add fcc-beta functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+app.use(express.static('public'))
 var months = [
   "January", "February", "March", "April",
   "May", "June", "July", "August",
@@ -9,7 +10,7 @@ var months = [
 var flag = true; // true -> unix timestamp; false -> natural language
 
 app.get("/", function(req, res){
-  res.send('Timestamp microservice. \nType a Unix timestamp (seconds) or natural language form of the date (MMM DD, YYYY) after the current URL /yourInputHere.');
+  res.render('index');
 });
 
 app.get("/:strDate", function(req, res){
@@ -38,7 +39,61 @@ app.get("/:strDate", function(req, res){
       return res.send(JSON.stringify({ unix: null, natural: null }, null, '\t'));
     }
 
-    return res.send(JSON.stringify({ unix: unixDate, natural: months[mon] + " " + day + ", " + year }, null, '\t'));
+    return res.send(JSON.stringify({ unix: Number(unixDate), natural: months[mon] + " " + day + ", " + year }, null, '\t'));
+  }
+});
+
+app.get("/api/timestamp/", function(req,res){
+  var milliseconds = Date.now();
+  var today = new Date(milliseconds);
+  var UTCstring = today.toUTCString();
+  return res.send(JSON.stringify({ unix: milliseconds, utc: UTCstring }, null, '\t'));
+});
+
+app.get("/api/timestamp/:strDate", function(req,res){
+  var strDate = req.params.strDate;
+  checkLength(strDate); // checks if input is unix timestamp (number) or date string
+
+  if (flag){ // unix timestamp
+    var date = new Date(Number(strDate)); // ms
+    var UTCstring = date.toUTCString();
+    if (!Number(strDate)){
+      return res.send(JSON.stringify({ error: "Invalid Date" }, null, '\t'));
+    }
+    return res.send(JSON.stringify({ unix: Number(strDate), utc: UTCstring }, null, '\t'));
+    
+  } else { // date string
+    var day = strDate.split('-')[2];
+    var mon = strDate.split('-')[1] - 1; // months off by 1 for 0 - 11
+    var year = strDate.split('-')[0];
+    console.log(year);
+    if (year.length === 1){ // force year to UTC 1900+
+      year = "190" + year;
+    }
+    if (year.length === 2){
+      year = "19" + year; // force year to UTC 1900+
+    }
+    while (year.length < 4){
+      year = "0" + year;
+    }
+    console.log(year);
+    var ISOString = (new Date (Date.UTC(year, mon, day))).toISOString().split('T')[0];
+    var UTCstring = (new Date (Date.UTC(year, mon, day))).toUTCString();
+    var unixDate = Date.parse(UTCstring);
+
+    mon += 1; // for months 1 - 12
+    if (mon < 10){ // zero padding
+      mon = "0" + mon;
+    }
+    if (day < 10){ // zero padding
+      day = "0" + day;
+    }
+console.log(ISOString, year + '-' + mon + '-' + day);
+    if (ISOString !== year + '-' + mon + '-' + day){
+      return res.send(JSON.stringify({ error: "Invalid Date" }, null, '\t'));
+    }
+
+    return res.send(JSON.stringify({ unix: unixDate, utc: UTCstring }, null, '\t'));
   }
 });
 
@@ -51,6 +106,13 @@ function checkFlag(str){
       return flag = false;
   }
   return flag = true; // first input is a number
+}
+
+function checkLength(str){
+  if (/\d\d?\d?\d?-\d\d?-\d\d?/g.test(str)){ // input format ####-#(#)-#(#)
+      return flag = false;
+  }
+  return flag = true; // input doesn't include hyphens
 }
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
